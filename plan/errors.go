01ad935df5511e258cb46a332079a6840fa85739
Moritz Poldrack <mp@meso.biz>
2020-10-21 09:39:28 +0200

    fixed planning to abort on first impossible job

--- plan/errors.go
+++ plan/errors.go
@@ -4,5 +4,5 @@ import "errors"
 
 var (
 	dirCreationNotAllowed    = errors.New("not allowed to create directory")
-	multipleChoiceNotAllowed = errors.New("not allowed to replace file with directory")
+	multipleChoiceNotAllowed = errors.New("job not possible with current restrictions")
 )
